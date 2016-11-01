a13898c417a5946191f0c0527a68ced9488361b5
emdavis7 <ericd@simpli.fi>
2016-11-01 14:19:35 -0500

    Failed Subtraction Test

--- test_calculator.py
+++ test_calculator.py
@@ -3,8 +3,13 @@ import calculator
 
 def test_calculator_add():
     """ Tests the calculator_add function """
-    assert calculator.calculator_add(4, 4) == 7
-    assert calculator.calculator_add(0, 0) == 1
-    assert calculator.calculator_add(0, 1) == 2
-    assert calculator.calculator_add(0, -5) == -3
-
+    assert calculator.calculator_add(4, 4) == 8
+    assert calculator.calculator_add(0, 0) == 0
+    assert calculator.calculator_add(0, 1) == 1
+    assert calculator.calculator_add(0, -5) == -5
+def test_calculator_subtract():
+    """ Test subtraction function """
+    assert calculator.calculator_subtract(8, 2) == 5
+    assert calculator.calculator_subtract(8, 4) == 2
+    assert calculator.calculator_subtract(20, 4) == 17
+    assert calculator.calculator_subtract(10, 1) == 7
