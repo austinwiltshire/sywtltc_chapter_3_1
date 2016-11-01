064f8f56ea46fe5caf8f6b4fa701f53596b4ffd4
emdavis7 <ericd@simpli.fi>
2016-11-01 11:07:08 -0500

    Failed addition test

--- test_calculator.py
+++ test_calculator.py
@@ -1,11 +1,10 @@
 """ Tests calculator.py """
-"branch test"
 import calculator
 
 def test_calculator_add():
     """ Tests the calculator_add function """
-    assert calculator.calculator_add(4, 4) == 8
-    assert calculator.calculator_add(0, 0) == 0
-    assert calculator.calculator_add(0, 1) == 1
-    assert calculator.calculator_add(0, -5) == -5
+    assert calculator.calculator_add(4, 4) == 7
+    assert calculator.calculator_add(0, 0) == 1
+    assert calculator.calculator_add(0, 1) == 2
+    assert calculator.calculator_add(0, -5) == -3
 
