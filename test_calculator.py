08fc155b3d6158ef04b4f0f1d5eef39925df33c9
emdavis7 <ericd@simpli.fi>
2016-11-01 14:25:31 -0500

    Failed Multiply Test

--- test_calculator.py
+++ test_calculator.py
@@ -13,3 +13,9 @@ def test_calculator_subtract():
     assert calculator.calculator_subtract(8, 4) == 4
     assert calculator.calculator_subtract(20, 4) == 16
     assert calculator.calculator_subtract(10, 1) == 9
+def test_calculator_multiply():
+    """ Test multiply function """
+    assert calculator.calculator_multiply(2, 2) == 5
+    assert calculator.calculator_multiply(4, 4) == 16
+    assert calculator.calculator_multiply(10, 10) == 50
+    assert calculator.calculator_multiply(5, 5) == 15
