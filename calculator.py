f8ad1f8ccd62e07081bbaf484e613b17085a9e72
emdavis7 <ericd@simpli.fi>
2016-11-01 14:31:17 -0500

    Add multiply function, fail test

--- calculator.py
+++ calculator.py
@@ -3,3 +3,5 @@ def calculator_add(left_hand_number, right_hand_number):
     return left_hand_number + right_hand_number
 def calculator_subtract(left_hand_number, right_hand_number):
     return left_hand_number - right_hand_number
+def calculator_multiply(left_hand_number, right_hand_number):
+    return left_hand_number * right_hand_number
