fb42dd97c48e438d06519964e81ddb20ad1e1395
bjak <blake_jakobsson@utexas.edu>
2017-06-21 09:17:48 -0500

    New Calc Examples

--- calculator.py
+++ calculator.py
@@ -1,3 +1,15 @@
 def calculator_add(left_hand_number, right_hand_number):
     """ Adds left_hand_number to right_hand_number """
     return left_hand_number + right_hand_number
+
+def calculator_subtract(left_hand_number, right_hand_number):
+    """ Subtracts left_hand_number to right_hand_number """
+    return left_hand_number - right_hand_number
+
+def calculator_multiply(left_hand_number, right_hand_number):
+    """ Multiplies left_hand_number to right_hand_number """
+    return left_hand_number * right_hand_number
+
+def calculator_divide(left_hand_number, right_hand_number):
+    """ Divides left_hand_number to right_hand_number """
+    return left_hand_number / right_hand_number
--- test_calculator.py
+++ test_calculator.py
@@ -9,21 +9,21 @@ def test_calculator_add():
     assert calculator.calculator_add(0, 1) == 1
     assert calculator.calculator_add(0, -5) == -5
 
-def test_calculator_multiply2():
+def test_calculator_multiply():
     """ Tests calculator_multiply function """
     assert calculator.calculator_multiply(2, 6) == 12
     assert calculator.calculator_multiply(5, 7) == 35
     assert calculator.calculator_multiply(4, 3) == 12
     assert calculator.calculator_multiply(5, 5) == 25
 
-def test_calculator_subtract2():
+def test_calculator_subtract():
     """ Tests calculator_subtract function """
     assert calculator.calculator_subtract(10, 5) == 5
     assert calculator.calculator_subtract(9, 6) == 3
     assert calculator.calculator_subtract(10, -20) == 30
     assert calculator.calculator_subtract(-10, 5) == -15
 
-def test_calculator_divide2():
+def test_calculator_divide():
     """ Test calculator_divide fucntion """
     assert calculator.calculator_divide(10, 2) == 5
     assert calculator.calculator_divide(12, 6) == 2
